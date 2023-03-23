07e00c9d2f3a00651e969c8821738d0a2c2adf44
Eliasvintra <rbnvintra.ke@gmail.com>
2023-03-23 20:41:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    let matchingDrivers = [];
+  
+    for (let i = 0; i < drivers.length; i++) {
+      if (drivers[i].toLowerCase() === name.toLowerCase()) {
+        matchingDrivers.push(drivers[i]);
+      }
+    }
+  
+    return matchingDrivers;
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    let matchingDrivers = [];
+  
+    for (let i = 0; i < drivers.length; i++) {
+      if (drivers[i].startsWith(letters)) {
+        matchingDrivers.push(drivers[i]);
+      }
+    }
+  
+    return matchingDrivers;
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
